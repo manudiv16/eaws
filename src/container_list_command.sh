5f1e18fab35345a7f7b43027f6cccccd16013b42
Fran <38869988+manudiv16@users.noreply.github.com>
2022-10-11 23:54:35 +0200

    refactor

--- src/container_list_command.sh
+++ src/container_list_command.sh
@@ -2,8 +2,17 @@
 check_profile 
 
 # Get cluster
-cluster=$(aws ecs list-clusters |jq -r '.clusterArns[]'| awk -F'/' '{print $2}'| fzf)
+cluster=$( aws ecs list-clusters \
+		| jq -r '.clusterArns[]' \
+		| awk -F'/' '{print $2}' \
+		| fzf )
+
 # Get service
-aws ecs list-services --cluster "${cluster}" | jq '.serviceArns[]' | awk -F'/' '{print $3}'| sed 's/"//g' | awk 'NF'  
+aws ecs list-services \
+		--cluster "${cluster}" \
+		| jq '.serviceArns[]' \
+		| awk -F'/' '{print $3}' \
+		| sed 's/"//g' \
+		| awk 'NF'  
 
 
